bd1f0ed7a14b727a3ef85de6a021a97976f6c180
Chris Wells <iroh@apache.org>
2022-12-28 15:59:34 -0500

    Added logic to download texture packs

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
+if [ ! -z "${APP_DIR}/jars/client-${MC_VERSION}.jar" ]; then
+    wget https://overviewer.org/textures/$MC_VERSION -O $APP_DIR/jars/client-$MC_VERSION.jar
+fi
+
 exec "$0"
